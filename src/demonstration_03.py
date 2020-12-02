c8f72a7baa63c29f94f8df93261079adadfd2c4f
Seth Nelson <seth.nelson@outlook.com>
2020-12-01 17:31:04 -0800

    completed problem 3

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -9,5 +9,7 @@ Examples:
 - string_int("12") ➞ 12
 """
 def string_int(txt):
-    # Your code here
+    return int(txt)
 
+print(string_int('6'))
+print(isinstance(string_int('6'), int)) # checks if the input is an int, return boolean value
